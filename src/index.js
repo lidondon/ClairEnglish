bd63fa75c8f355261b112a031f2f27e747ccd60a
lidondon <lidon720608@gmail.com>
2024-03-21 00:11:48 +0800

    fix(substring issue): not startwith but includes. remove .pem

--- src/index.js
+++ src/index.js
@@ -3,15 +3,8 @@ import fs from "fs";
 import config from "./config/config";
 import app from "./config/express";
 
-const privateKey = fs.readFileSync("key.pem", "utf8");
-const certificate = fs.readFileSync("cert.pem", "utf8");
-const credentials = {
-    key: privateKey,
-    cert: certificate
-};
-
-https.createServer(credentials, app).listen(config.port, () => {
-    console.log("https server listening on port 3000");
+app.listen(config.port, () => {
+    console.log(`server started on  port https://127.0.0.1:${config.port} (${config.env})`);
 });
 
 export default app;
